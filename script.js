46fdb1527940a8ee6255f2552bb23693960d9034
Charlie Turner <c.w.turner@hotmail.com>
2024-07-10 19:47:42 +0100

    Add initial async check weather function and verify function via console log

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const apiKey = "64ad61cb3f2845bfb21163612240507";
-const URL =
+const apiURL =
   "http://api.weatherapi.com/v1/current.json?key=64ad61cb3f2845bfb21163612240507&q=London&aqi=no";
+
+async function checkWeather() {
+  const response = await fetch(apiURL + `&appid${apiKey}`);
+  let data = await response.json();
+
+  console.log(data);
+}
+
+checkWeather();
