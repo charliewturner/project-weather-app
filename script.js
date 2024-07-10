94a3500d68c5f9fa5b917c74384e8c1e81d68daf
Charlie Turner <c.w.turner@hotmail.com>
2024-07-10 19:43:46 +0100

    Define template URL and API Key variables

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("Hello world!");
+const apiKey = "64ad61cb3f2845bfb21163612240507";
+const URL =
+  "http://api.weatherapi.com/v1/current.json?key=64ad61cb3f2845bfb21163612240507&q=London&aqi=no";
