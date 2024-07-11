673a950fad84983969dcf52cbfe0d70fdcc93053
Charlie Turner <c.w.turner@hotmail.com>
2024-07-11 22:22:05 +0100

    Alter API URL for 6 day forecast. Add 6 divs for forecast visual

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "64ad61cb3f2845bfb21163612240507";
 const apiURL =
-  "http://api.weatherapi.com/v1/forecast.json?key=64ad61cb3f2845bfb21163612240507&q=London&days=3&aqi=no&alerts=yes";
+  "http://api.weatherapi.com/v1/forecast.json?key=64ad61cb3f2845bfb21163612240507&q=London&days=7&aqi=no&alerts=yes";
 
 async function checkWeather() {
   const response = await fetch(apiURL + `&appid${apiKey}`);
