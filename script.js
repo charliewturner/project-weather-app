cab95157886e72b1e68c93a5a1e60509c1ed87c6
Charlie Turner <c.w.turner@hotmail.com>
2024-07-11 23:37:59 +0100

    Add initial script to update fields with weather data

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ async function checkWeather() {
   const response = await fetch(apiURL + `&appid${apiKey}`);
   let data = await response.json();
 
+  document.querySelector(".weather-class").innerHTML =
+    data.current.condition.text;
+  document.querySelector(".weather-symbol").innerHTML =
+    data.current.condition.icon;
+  document.querySelector(".temp-actual").innerHTML =
+    "Temperature: " + data.current.temp_c;
+  document.querySelector(".temp-feel").innerHTML =
+    "Feels like: " + data.current.feelslike_c;
+  document.querySelector(".uv-index").innerHTML =
+    "UV Index: " + data.current.uv;
+
+  document.querySelector(".wind-speed").innerHTML =
+    "Wind speed: " + data.current.wind_kph;
+  document.querySelector(".wind-direction").innerHTML =
+    "Wind direction: " + data.current.wind_dir;
+
+  document.querySelector(".location").innerHTML = "" + data.location.name;
+  document.querySelector(".local-time").innerHTML =
+    "" + data.location.localtime;
+
   console.log(data);
 }
 
