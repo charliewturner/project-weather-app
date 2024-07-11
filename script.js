0c41d3103b1622e6b9a7c07ab95bc5027f2a4a3c
Charlie Turner <c.w.turner@hotmail.com>
2024-07-11 23:53:27 +0100

    Upload progress before stopping

--- script.js
+++ script.js
@@ -16,15 +16,34 @@ async function checkWeather() {
     "Feels like: " + data.current.feelslike_c;
   document.querySelector(".uv-index").innerHTML =
     "UV Index: " + data.current.uv;
-
   document.querySelector(".wind-speed").innerHTML =
     "Wind speed: " + data.current.wind_kph;
   document.querySelector(".wind-direction").innerHTML =
     "Wind direction: " + data.current.wind_dir;
+  document.querySelector(".location").innerHTML = data.location.name;
+  document.querySelector(".local-time").innerHTML = data.location.localtime;
 
-  document.querySelector(".location").innerHTML = "" + data.location.name;
-  document.querySelector(".local-time").innerHTML =
-    "" + data.location.localtime;
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const d1 = new Date();
+  let day = weekday[d1.getDay()];
+  document.getElementById("forecast-day-one").innerHTML = day;
+
+  //write switch-case to alter teh following divs depending on the
+  //current day shown in the forecast-day-one div
+  document.getElementById("forecast-day-two").innerHTML = day;
+  document.getElementById("forecast-day-three").innerHTML = day;
+  document.getElementById("forecast-day-four").innerHTML = day;
+  document.getElementById("forecast-day-five").innerHTML = day;
+  document.getElementById("forecast-day-six").innerHTML = day;
 
   console.log(data);
 }
