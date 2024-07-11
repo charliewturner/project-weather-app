4119ae8cafc56a870b90ef877e7e2936156eb959
Charlie Turner <c.w.turner@hotmail.com>
2024-07-11 21:27:22 +0100

    Change API url to one including forecast in the data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "64ad61cb3f2845bfb21163612240507";
 const apiURL =
-  "http://api.weatherapi.com/v1/current.json?key=64ad61cb3f2845bfb21163612240507&q=London&aqi=no";
+  "http://api.weatherapi.com/v1/forecast.json?key=64ad61cb3f2845bfb21163612240507&q=London&days=3&aqi=no&alerts=yes";
 
 async function checkWeather() {
   const response = await fetch(apiURL + `&appid${apiKey}`);
