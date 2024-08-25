2139ec4047b783cf6f56c1bbdb3834e3d0263d08
Charlie Turner <c.w.turner@hotmail.com>
2024-08-25 20:13:30 +0100

    Alter URL variable to allow for search

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const apiKey = "64ad61cb3f2845bfb21163612240507";
-const apiURL =
-  "http://api.weatherapi.com/v1/forecast.json?key=64ad61cb3f2845bfb21163612240507&q=London&days=7&aqi=no&alerts=yes";
+let apiURL =
+  "http://api.weatherapi.com/v1/forecast.json?key=64ad61cb3f2845bfb21163612240507&q=" +
+  location +
+  "&days=7&aqi=no&alerts=yes";
 
 async function checkWeather() {
   const response = await fetch(apiURL + `&appid${apiKey}`);
